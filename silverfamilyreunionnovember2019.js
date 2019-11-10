e6e93e2fe901d6c998b4792cf0064352e0d477a6
Craig Silver <silver.craig@gmail.com>
2019-11-10 17:32:13 -0500

    Update silverfamilyreunionnovember2019.js

--- silverfamilyreunionnovember2019.js
+++ silverfamilyreunionnovember2019.js
@@ -2,8 +2,8 @@ var app = angular.module('photos', []);
 app.controller('PhotosController', function TrumpController($scope, $timeout) {
 	$scope.subalbums = [
 		{
-		"link": "",
-		"img": "",
+		"link": "https://photos.app.goo.gl/stHFovEQrmerthY59",
+		"img": "https://lh3.googleusercontent.com/hW5DIkG7wEqI9uEHR-dpF5llnwr2hwXozIi7iZ98WrEZbirtW1Fu1AvCbP3yE3b-JUH2FGWKCWtwIYzqaBh_z0OjsWiMfOXPKajTThKFwMMgCkHVLyZ1klKlG6IZouq5D7pEh0N9YfWc52PFBLManHdUaBAyOw1Y4zb15Oes6wvIKhu1pnPFHVpXfOsufPgwFB6FSYf4GRGFujHjVuHViszlX16KOdMoCYTxBEcycVEIqXomeaBryi1qrPAICn0_08fI1f2K9lCXXoz0puNhvN-8di3UVg296irIkxMPYsiBBgz5B-P_EV8iPr0KsOp8R0qW6bviBd6wCTwWxz5wb2Glhk1EYl52WrkfQSIQqypKgQNsTP1IY0ADTr6HQRuXGkLo9aUdA9WXfZQejWO-ySLJzt8kOoc6IwwfArvX7XzeG71FCn2qmIFGnwvHRgBxdhqYI6Nylchtb-Oj3RaeIX4jU2QJpVTGB71Zy8ykkLNbTMR5sRzB7-Ob2HzHr-P0n9VxU0pn4x8I08Qpd44TkUSboIQSF5RqBDNTgSdMvoqpY8jKaO7k0OVqhcvQ983oTd_EkEDw5Wq4X7nSk3ayPD8RreDfoQtGFvjYS_xdOxVUujbg7iRE5KIsWGYCzu8Rn8KlHASU9YpUHeUSCdNHFVxxw8ZXGMNzkt4MgL1OoZFx6jKWqN4XbjXC=w242-h323-no",
 		"title": "Lisa",
  		"date": new Date("10/5/2019")
 	},{
@@ -13,8 +13,8 @@ app.controller('PhotosController', function TrumpController($scope, $timeout) {
  		"date": new Date("10/5/2019")
 	},
 		{
-		"link": "",
-		"img": "",
+		"link": "https://photos.google.com/share/AF1QipPVWN5nuZk7cIIuL7_bgJ-bDCvz8vAPGo76EUxge2fvS7qauY8jDYoCWCr7QsLVuw?key=UnQwS0FtdUo4dFlCQkpKVVFmenJZS2JuREJZbmdR",
+		"img": "https://lh3.googleusercontent.com/nVxF5bBhCIOPZm8N2nis1MPCioYn9KRkXVSsn9PZd4wMqvQuOGPYgxuMfeVH3yLNZn-_pekB0h8wOIOgd6XVQsCB4fFRiKGhXvjY6AX1QMcNtIkAg9CJvvxajVTsD-NgIR-mokyxRzPJgcqVBLB0FIQu2NILce941zZzko227D4UjlIzLWnnGkZ_T8uXbEUfVSjDieUgLOxlb8BFQI_JunAjpIDElVu7rwoYI7UmuHxkMZgyPNJjUu4bp0xZUDKyae4b8H-zqr8SK2M4DttaswE-QgUbIOU94xN1Yhrvmza7KQLhA_zOf6LamdH4n-sALi2C1Ri3S5GSBVuxZOqFKxcBbYaX0WPdUwlz_LJL-e4b1IHIv3i3dJTs_tXMD6rUuGY7lUG4FgHnquAmvWvpphWxuWjDbriANG6GPclyekWSaKx5pSlRV15cqUtKee58Y1pngTrBfiTcP6Y2HLS7PVwkPG_6VI0LpUhKl1XnjPL_gXEfuJzY2vvc16750qYYhplE_QMkyxtw2Vm_X-2iUpXcl6m3B2p1Sn43jJz3f0gkvE1yvrGK8elvEWG0Qp8SXGG0MtPHmHrJIoQoAYlZ6h9zf6QIW4-b_pP0sFzPd_hDry0K_Llqr6wHD8T3bFo9OPbd2_SUa8wgM82wJb5D9tvk1KJ_jSOslQ0ljTzVbkgEvFg9TqZnLp7GdJYIwMIjTIV0s5SBb1A9EluYt9iBd8z8LKmKuHal4JVlmPlRJ2gcdcdo2A=w461-h346-no",
 		"title": "Julie and Callan",
  		"date": new Date("6/28/2019")
 	}
